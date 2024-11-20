329a2dce40a8e85d51aa1d7b96cf5b36f78f2fde
tuleshwar-tiwari <72892030+tuleshwar-tiwari@users.noreply.github.com>
2024-11-20 13:35:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript to add interactivity
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("clickMe");
+    button.addEventListener("click", function () {
+        alert("Hello! You clicked the button!");
+    });
+});
